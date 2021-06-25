5506b3ac54759aaedc5d77e3faf99843464cf693
M0hamm3d 8a19 <54952402+Mohammed-Baig@users.noreply.github.com>
2021-06-24 22:22:57 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import numpy as np
+import sys
+
 def InputMatrix(R,C):
     matrix = []
     print("Enter the entries rowwise:")
@@ -11,9 +14,40 @@ def InputMatrix(R,C):
   
     return matrix
 
-def GaussJordan(matrix):
+def GaussJordan(n):
+    a = np.zeros((n,n+1))
+    x = np.zeros(n)
+    print('Enter Augmented Matrix Coefficients:')
+    for i in range(n):
+        for j in range(n+1):
+            a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
     
-    return
+    for i in range(n):
+        if a[i][i] == 0.0:
+            sys.exit('Divide by zero detected!')
+         
+        for j in range(i+1, n):
+            ratio = a[j][i]/a[i][i]
+         
+            for k in range(n+1):
+                a[j][k] = a[j][k] - ratio * a[i][k]
+ 
+    x[n-1] = a[n-1][n]/a[n-1][n-1]
+ 
+    for i in range(n-2,-1,-1):
+        x[i] = a[i][n]
+     
+        for j in range(i+1,n):
+            x[i] = x[i] - a[i][j]*x[j]
+     
+        x[i] = x[i]/a[i][i]
+    print("The matrix in REF is:")
+    print(a)
+
+    print('\nThe solution is: ')
+    for i in range(n):
+        print('X%d = %0.2f' %(i,x[i]), end = '\t')
+    
 
 def ScalarMultiplication(matrix,scalar,R,C):
     matrixNew = []
